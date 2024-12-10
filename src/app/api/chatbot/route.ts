a716e903a8c9b8ed439f101696c541593f2fb360
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-12-10 12:05:25 +0200

    create api for chatbot

---
+++ src/app/api/chatbot/route.ts
@@ -0,0 +1,36 @@
+import Groq from "groq-sdk";
+import { NextRequest, NextResponse } from "next/server";
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const { message } = await req.json();
+    if (!message)
+      return NextResponse.json(
+        { message: "Message content is required" },
+        { status: 400 }
+      );
+
+    const chat = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: "Explain the importance of fast language models",
+        },
+      ],
+      model: "llama3-8b-8192",
+    });
+    const response =
+      chat.choices[0].message.content || "Sorry, No response from llama";
+    return NextResponse.json({ message: response });
+  } catch (error) {
+    console.error("Error in chat api", error);
+    return NextResponse.json(
+      { message: "Error happened while processing your request" },
+      { status: 500 }
+    );
+  }
+}
