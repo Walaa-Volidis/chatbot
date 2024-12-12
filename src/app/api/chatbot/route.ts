714973680648b282b16f9deab17ed18a3e15df79
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-12-12 08:55:43 +0200

    using zod

--- src/app/api/chatbot/route.ts
+++ src/app/api/chatbot/route.ts
@@ -1,31 +1,35 @@
 import Groq from "groq-sdk";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 });
-
+const ZMessageSchema = z.object({
+  message: z.string(),
+});
 export async function POST(req: NextRequest) {
   try {
-    const { message } = await req.json();
-    if (!message)
+    const body = await req.json();
+    const parsedMessage = ZMessageSchema.safeParse(body);
+    if (!parsedMessage.success)
       return NextResponse.json(
         { message: "Message content is required" },
         { status: 400 }
       );
-
+    const { message } = parsedMessage.data;
     const chat = await groq.chat.completions.create({
       messages: [
         {
           role: "user",
-          content: "Explain the importance of fast language models",
+          content: message,
         },
       ],
       model: "llama3-8b-8192",
     });
     const response =
       chat.choices[0].message.content || "Sorry, No response from llama";
-    console.log("hey response", response);
+    ZMessageSchema.parse({ message: response });
     return NextResponse.json({ response });
   } catch (error) {
     console.error("Error in chat api", error);
